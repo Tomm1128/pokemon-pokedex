9fb0c2d2fbddde1237bfbcfb12f3163802f61900
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-28 11:56:52 -0400

    Display pokedex background image with pokemon sprite

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const randomPokemon = Math.floor(Math.random() * 100)
 
 const displayPokemon = (pokemon) => {
-  const pokemonSection = document.getElementById("pokemon-details")
+  const pokemonSection = document.getElementById("pokedex")
+  const pokemonSprite = pokemon.sprites.front_default
+  const img = document.createElement("img")
+  img.id = "sprite"
+  img.src = pokemonSprite
+  pokemonSection.appendChild(img)
+  // debugger
 }
 
 const getPokemon = (id = randomPokemon) => {
