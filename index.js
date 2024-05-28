fc287beacf41cb48a7bc93aef69e2cdceaeaa523
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-28 13:21:56 -0400

    Update styling and size of images

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const randomPokemon = Math.floor(Math.random() * 100)
 
 const displayPokemon = (pokemon) => {
   const pokemonSection = document.getElementById("pokedex")
-  const pokemonSprite = pokemon.sprites.front_default
   const img = document.createElement("img")
+  const h3 = document.createElement("h3")
+  const p = document.createElement("p")
+  const span = document.createElement("span")
+
+  const pokemonSprite = pokemon.sprites.front_default
+  const pokemonName = pokemon.name
+
   img.id = "sprite"
   img.src = pokemonSprite
+
   pokemonSection.appendChild(img)
-  // debugger
 }
 
 const getPokemon = (id = randomPokemon) => {
