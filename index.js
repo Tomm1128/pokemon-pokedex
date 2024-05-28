1048ff7503e07ded002c2ae67f97eab5e1ced006
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-28 13:25:45 -0400

    Remove unused variables

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const randomPokemon = Math.floor(Math.random() * 100)
 const displayPokemon = (pokemon) => {
   const pokemonSection = document.getElementById("pokedex")
   const img = document.createElement("img")
-  const h3 = document.createElement("h3")
-  const p = document.createElement("p")
-  const span = document.createElement("span")
 
   const pokemonSprite = pokemon.sprites.front_default
-  const pokemonName = pokemon.name
 
   img.id = "sprite"
   img.src = pokemonSprite
