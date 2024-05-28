e61ea772f3505fd96682d8173717e6cdfe5b11c1
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-28 14:20:32 -0400

    Update pokemon on DOM upon user input submission

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const randomPokemon = Math.floor(Math.random() * 100)
 
+const handleUserInput = (event) => {
+  event.preventDefault()
+  const userInput = event.target[0].value
+  getPokemon(userInput)
+}
+
 const displayPokemon = (pokemon) => {
-  const pokemonSection = document.getElementById("pokedex")
+  const pokedexSection = document.getElementById("pokedex")
+  const detailsSection = document.getElementById("pokedex-details")
+
+  pokedexSection.innerHTML = " "
+  detailsSection.innerHTML = " "
+
   const img = document.createElement("img")
+  const h2 = document.createElement("h2")
+  const p = document.createElement("p")
+  const span = document.createElement("span")
 
   const pokemonSprite = pokemon.sprites.front_default
+  const pokemonName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+
+  h2.textContent = pokemonName
 
   img.id = "sprite"
   img.src = pokemonSprite
 
-  pokemonSection.appendChild(img)
+  pokedexSection.appendChild(img)
+  detailsSection.appendChild(h2)
 }
 
 const getPokemon = (id = randomPokemon) => {
@@ -19,7 +37,10 @@ const getPokemon = (id = randomPokemon) => {
 }
 
 const init = () => {
+  const pokemonSearchForm = document.getElementById("pokemon-search")
   getPokemon()
+
+  pokemonSearchForm.addEventListener("submit", handleUserInput)
 }
 
 
